ebc2a371ad912134fe4f811b2a4ca999f51c2254
magnusten <modestas.slivinskas@gmail.com>
2020-04-20 22:19:49 +0300

    implemented getting inputs

--- app.js
+++ app.js
@@ -3,16 +3,43 @@ var budgetController = (function() {
 })();
 
 
-var IUController = (function() {
+var UIController = (function() {
+
+	var DOMstrings = {
+		inpuType : '.add__type',
+		inputDescription : '.add__description',
+		inputValue : '.add__value',
+		inputButton : '.add__btn'
+	}
+
+
+	return {
+		getInput : function() {
+			return {
+				type : document.querySelector(DOMstrings.inpuType).value, // Will be inc or exp
+				description : document.querySelector(DOMstrings.inputDescription).value,
+				value : document.querySelector(DOMstrings.inputValue).value
+			}
+		},
+
+		getDOMstrings : function() {
+			return DOMstrings;
+		}
+	}
 
 })();
 
 
 
-var controller = (function(front,back) {
+var controller = (function(UICtrl, budgetCtrl) {
+
+	var DOM = UICtrl.getDOMstrings();
 
 	var controlAddItem = function() {
+
 		//1. Get the filled input data
+		var input = UICtrl.getInput();
+		console.log(input);
 
 		//2. Add the item to the budget controller
 
@@ -23,7 +50,7 @@ var controller = (function(front,back) {
 		//5. Display the budget on the UI
 	}
 
-	document.querySelector('.add__btn').addEventListener('click', controlAddItem);
+	document.querySelector(DOM.inputButton).addEventListener('click', controlAddItem);
 
 	document.addEventListener('keypress', function(event) {
 		if(event.keyCode === 13 || event.which === 13) {
@@ -31,4 +58,4 @@ var controller = (function(front,back) {
 		}
 	});
 
-})(IUController, budgetController);
+})(UIController, budgetController);
